6ba449991baa6b935b4d886d84a7625e043862d7
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2022-02-21 22:29:09 +0530

    Time: 9 ms (29.73%), Space: 13.8 MB (47.22%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    
+    void Solve(int n, string str, int O, int C, vector<string> &ans){
+        
+        if(O == n && C ==n){
+            ans.push_back(str);
+        }
+        
+        if( O < n){
+            Solve(n, str + '(', O+1, C, ans);
+        }
+        
+        if( C < O){
+            Solve(n, str + ')', O, C+1, ans);
+        }
+    }
+    
+    vector<string> generateParenthesis(int n) {
+        
+        vector<string> ans;
+        
+         Solve(n, "", 0, 0, ans);
+        return ans;
+        
+    }
+};
