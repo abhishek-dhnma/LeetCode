fdfe4f2597df99a89ab0dc6dfc9d7ff774f6fb44
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2022-06-21 23:26:11 +0530

    Time: 43 ms (5.17%), Space: 6 MB (25.06%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -2,6 +2,10 @@ class Solution {
 public:
     int mySqrt(int x) {
         
+        if(x == 0 || x == 1){
+            return x;
+        }
+        
         long i = 0;
         while(i*i <= x){
             if(i*i == x){
