bf5dd32d702fe3f7fab9641a77098902b793380e
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2022-06-23 00:32:46 +0530

    Time: 56 ms (16.45%), Space: 19.2 MB (57.45%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         
+        // SWAPING TECHNIQUE
+        
         int n = nums.size();int j=0;
         for(int i=0; i<n; i++){
             if(nums[i] != 0){
