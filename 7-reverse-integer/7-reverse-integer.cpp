6363665edf4d26dd26713bb91996d68334fdaa7d
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2022-06-26 17:03:37 +0530

    Time: 3 ms (63.56%), Space: 6 MB (31.29%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.cpp
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -8,12 +8,9 @@ public:
             
             int d = x%10;
          x /= 10;
-       if (rev > INT_MAX/10 || (rev == INT_MAX / 10 && d > 7)) return 0;
+            if (rev > INT_MAX/10 || (rev == INT_MAX / 10 && d > 7)) return 0;
             if (rev < INT_MIN/10 || (rev == INT_MIN / 10 && d < -8)) return 0;
             rev = rev * 10 + d;
-        
-        
-           
             
         }
 
