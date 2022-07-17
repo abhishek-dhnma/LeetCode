653f49c092f5c53a606aa1df09876d310b44283d
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2022-07-18 00:57:05 +0530

    Time: 31 ms (16.26%), Space: 18.4 MB (74.77%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -6,10 +6,7 @@ public:
         int n = nums.size();
         int ans = 1;
 
-        if(n < 2){
-            cout << n;
-        }
-        
+      
         for(int j=1; j<n; j++){
             if(nums[k] != nums[j]){
                 k++;
