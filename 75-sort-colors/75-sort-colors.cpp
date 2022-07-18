d92670ed227cbf52218f0eb37e768d1a925c3055
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2022-07-18 23:28:13 +0530

    Time: 8 ms (11.62%), Space: 8.3 MB (23.39%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -5,7 +5,7 @@ public:
         
         int flag = false;
         
-        for(int i=1; i<=n; i++){
+        for(int i=1; i<=n-1; i++){
 
         for(int j=0; j<n-i; j++){
             if(nums[j] > nums[j+1]){
