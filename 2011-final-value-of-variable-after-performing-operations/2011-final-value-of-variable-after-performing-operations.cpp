3cd4ad0013e881a547cf8841e012dc994b70e98a
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2022-07-21 23:43:54 +0530

    Time: 18 ms (28.38%), Space: 13.9 MB (92.90%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
@@ -3,11 +3,11 @@ public:
     int finalValueAfterOperations(vector<string>& operations) {
         int x = 0;
         for(int i=0; i<operations.size(); i++){
-            if(operations[i] == "--X" or operations[i] == "X--"){
-                x -= 1;
-            }
-            else if(operations[i] == "X++" or operations[i] == "++X"){
+            if(operations[i][0] == '+' or operations[i][1] == '+'){
                 x += 1;
+            }
+            else {
+                x -= 1;
             }
             
         }
