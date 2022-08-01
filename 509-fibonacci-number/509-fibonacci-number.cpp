ab821146722a22cc17f5c5683180f140cd74b647
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2022-08-01 23:56:49 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (78.78%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -3,11 +3,25 @@ public:
     int fib(int n) {
         
         
-        if(n==0 || n==1){
-            return n;
+        int first = 0;
+        int second = 1;
+        int third;
+        
+        if(n == 0){
+            return 0;
         }
+        if (n==1){
+            return 1;
+        }
+        
+        for(int i=1; i<n; i++){
             
-            return fib(n-1) + fib(n-2);
+             third = first + second ;
+            first = second;
+                second = third;
+        }
+        
+        return third;
         
     }
 };
