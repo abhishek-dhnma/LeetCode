1bc048f21ee0a1791bfde58d1ae449b07ec2fcf0
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2022-08-02 00:28:50 +0530

    Time: 4 ms (41.35%), Space: 5.9 MB (26.52%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -5,15 +5,10 @@ public:
     bool isPowerOfTwo(int n) {
         
         
-        while( (n>0) and (n%2 == 0)){
-        n = n>>1;
-    }
+        if(n>0 and (n&(n-1)) == 0){
+            return true;
 
-    if(n==1){
-        return true;
-    }
-     
-        return false;
-
+        }
+return false;
     }
 };
