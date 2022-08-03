8b7eef713cc9996c99a181cbf74d6a2e9fee6cc9
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2022-08-04 01:21:38 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (72.55%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.cpp
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -6,61 +6,42 @@ public:
         int m = matrix.size();
         int n = matrix[0].size();
         
-        int top = 0;
-        int down = m-1;
-        int left = 0;
-        int right = n-1;
-        int dir = 0;
+    int top = 0;
+    int down = m-1;
+    int left = 0;
+    int right = n-1;
+    int d = 0;
         
-        while(top <= down and left <= right){
-            
-            if(dir==0){
+    while(top <= down && left <= right){
         
-        for (int i = left ; i <= right ; i++)
-        {
-            ans.push_back(matrix[top][i]);
-        }
-        top++;
-        }
-        
-
-        // Top => Down
-
-        else if(dir==1){
-        
-            for (int i = top ; i <= down; i++)
-            {
+        // L -> R
+        if(d == 0){
+            for(int i=left; i<=right; i++){
+                ans.push_back(matrix[top][i]);
+            }
+            top++;
+            // T -> D
+        }else if(d == 1){ 
+            for(int i=top; i<=down; i++){
                 ans.push_back(matrix[i][right]);
             }
-            
             right--;
-        }
-        
-        //Right => Left  [ NOW WE GOING REVERSE SO i >= left not ]
-        else if(dir==2){
-        
-            for (int i = right; i >= left; i--)
-            {
+            // R -> L
+        }else if(d == 2){
+            for(int i=right; i>=left; i--){
                 ans.push_back(matrix[down][i]);
             }
-            
             down--;
-        }
-        
-
-        //Down => Top
-
-        else if(dir==3){
-
-            for (int i = down; i >= top; i-- )
-            {
+            // D -> T
+        }else if(d == 3){
+            for(int i=down; i>=top; i--){
                 ans.push_back(matrix[i][left]);
             }
             left++;
         }
-            dir = (dir+1)%4;
-        }
-        
+
+        d = (d+1)%4;
+    }
         return ans;
         
     }
