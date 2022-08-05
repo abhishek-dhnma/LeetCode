e83019d20b06c394b90193316128a2117196be7a
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2022-08-06 01:21:09 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (64.77%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
@@ -13,14 +13,13 @@ public:
     int dir = 0;
     int g=1;
 
-
     while(t<=d && l<=r){
         // l->r
         if(dir==0){
         for(int i=l; i<=r; i++){
             matrix[t][i] = g++;;
         }
-        dir = 1;
+        //dir = 1;
         t++;
         // t -> d
         }else if(dir==1){
@@ -28,22 +27,23 @@ public:
                matrix[i][r] = g++;;
             }
             r--;
-            dir = 2;
+           // dir = 2;
             // r -> l
         }else if(dir==2){
             for(int i=r; i>=l; i--){
                 matrix[d][i] = g++;;
             }
             d--;
-            dir = 3;
+          //  dir = 3;
             // d -> t
         }else if(dir==3){
             for(int i=d; i>=t; i--){
                 matrix[i][l] = g++;;
             }
             l++;
-            dir = 0;
+          //  dir = 0;
         }
+        dir = (dir+1)%4;
     }
         
         return matrix;
