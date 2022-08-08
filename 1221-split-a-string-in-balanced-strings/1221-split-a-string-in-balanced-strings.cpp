581d0662faa32d701c298ac0b00dc26b69c079b2
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2022-08-09 00:24:49 +0530

    Time: 4 ms (27.29%), Space: 6.3 MB (39.90%) - LeetHub

--- 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
@@ -6,19 +6,12 @@ public:
         int c1=0,c2=0;
         
             
-            for(int j=0; j < (s.substr(0,j)).size()+1; j++){
-                if(s[j] == 'L'){
-                    c1++;
+            for (int i=0; i<s.size(); i++){
+                if(s[i] == 'L') c1++;
+                else c2++;
                     
-                }else{
-                    c2++;
-                }
-            
-            if(c1 == c2){
-                ans++;
+                if(c1==c2) ans++;
             }
-            
-        }
         
         return ans;
         
