f39657a02f6186f1c674e7bae8d1ffa6c03159ee
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2022-08-11 00:52:47 +0530

    Time: 125 ms (5.04%), Space: 25.6 MB (5.09%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         
-        set<int> s;
+         set<int> s;
         int n = nums.size();
         for(int i=0; i<=n; i++){
             s.insert(i);
@@ -12,7 +12,7 @@ public:
             
             if(s.find(nums[i]) != s.end()){
                 s.erase(nums[i]);
-                i--;
+                
             }
         }
         
