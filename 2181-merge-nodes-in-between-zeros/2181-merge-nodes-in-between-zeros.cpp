769192f51811dfd5188fd8418eab166dc9fe75f0
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2022-08-17 00:47:59 +0530

    Time: 1129 ms (54.25%), Space: 274.6 MB (11.80%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cpp
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cpp
@@ -3,6 +3,7 @@
  * struct ListNode {
  *     int val;
  *     ListNode *next;
+ 
  *     ListNode() : val(0), next(nullptr) {}
  *     ListNode(int x) : val(x), next(nullptr) {}
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
@@ -10,23 +11,29 @@
  */
 class Solution {
 public:
+    
+    
     ListNode* mergeNodes(ListNode* head) {
-        ListNode* res = new ListNode(0);
-        ListNode* ret = res;
-        int sm = 0;
-        head = head->next;
         
-        while(head != NULL){
-            if(head->val == 0){
-                res->next = new ListNode(sm);
-                res = res->next;
-                sm = 0;
-            } else {
-                sm += head->val;
+        ListNode ans, *tail = &ans;
+        
+        while(head){
+                        
+            int sum = 0;
+            if(head->val == 0) head = head->next;
+            if(!head) break;
+            
+            while(head->val != 0){
+                
+                    sum = sum + head->val;
+                    head = head->next;
+                }
+             tail->next =   new ListNode(sum);
+            tail = tail->next;
+            
             }
-            head = head->next;
-        }
+            
+        return ans.next;
         
-        return ret->next;
-    }
+        }
 };
