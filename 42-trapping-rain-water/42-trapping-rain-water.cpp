07e1757f5cbc32826fc5be64b14fd70797e2f42d
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2022-08-24 01:30:02 +0530

    Time: 27 ms (45.70%), Space: 21.2 MB (5.38%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,47 +1,41 @@
 class Solution {
-public:
-    
-    int WaterTrapped(vector<int> building_Heights, int n){
+    public:
 
-    vector<int> lmax(n,0);
-    vector<int> rmax(n,0);
+        int WaterTrapped(vector < int > building_Heights, int n) {
 
-    int leftmaxsofar = building_Heights[0];
-    for(int i=0; i<n; i++){
-        lmax[i] = max(leftmaxsofar, building_Heights[i]);
-        leftmaxsofar = lmax[i];
-    }
+            vector < int > lmax(n, 0);
+            vector < int > rmax(n, 0);
 
+            int leftmaxsofar = building_Heights[0];
+            for (int i = 0; i < n; i++) {
+                lmax[i] = max(leftmaxsofar, building_Heights[i]);
+                leftmaxsofar = lmax[i];
+            }
 
+            int rightmaxsofar = building_Heights[n - 1];
+            for (int i = n - 1; i >= 0; i--) {
+                rmax[i] = max(rightmaxsofar, building_Heights[i]);
+                rightmaxsofar = rmax[i];
+            }
 
+            vector < int > ans(n, 0);
 
-        int rightmaxsofar = building_Heights[n-1];
-    for(int i=n-1; i>=0; i--){
-        rmax[i] = max(rightmaxsofar, building_Heights[i]);
-        rightmaxsofar = rmax[i];
-    }
+            for (int i = 1; i < n - 1; i++) {
 
+                ans[i] = min(lmax[i], rmax[i]) - building_Heights[i];
+            }
 
-    vector<int> ans(n, 0);
+            int TotalWaterTrappedBetweenBuildings = 0;
 
-    for(int i=1; i<n-1; i++){
+            for (auto xx: ans) {
+                TotalWaterTrappedBetweenBuildings += xx;
+            }
 
-        ans[i] = min(lmax[i], rmax[i]) - building_Heights[i];
-    }
+            return TotalWaterTrappedBetweenBuildings;
 
+        }
+    int trap(vector < int > & height) {
 
-    int TotalWaterTrappedBetweenBuildings = 0;
-
-    for(auto xx : ans){
-        TotalWaterTrappedBetweenBuildings += xx;
-    }
-
-    return TotalWaterTrappedBetweenBuildings;
-
-}
-    int trap(vector<int>& height) {
-        
-         
         return WaterTrapped(height, height.size());
     }
 };
