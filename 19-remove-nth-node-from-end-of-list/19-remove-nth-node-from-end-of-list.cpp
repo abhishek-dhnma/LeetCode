576c099c6bcb7a04a8b756cf0e404b22961a7c93
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2022-09-02 04:08:13 +0530

    Time: 9 ms (36.63%), Space: 10.5 MB (96.43%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -20,7 +20,7 @@ public:
             fast = fast->next;
         }
         
-        if(fast == NULL) return head->next;
+        if(fast == NULL) return head->next; // Edge Case handled when n = length of LL i.e. now n is first element of the LL. Example [1,2,3], n = 3. Here n is the length of the LL(Element which we want to remove is become the first element of the LL ) So fast become NULL as fast=fast->next n times so it become NULL at the end of the iteration and we can return head->next
         
         while( fast->next != NULL){
             fast = fast->next;
