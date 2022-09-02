b8ddbc615f12cfbec75fb6d5518191903eb895fd
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2022-09-02 05:37:56 +0530

    Time: 4 ms (74.25%), Space: 13.8 MB (36.83%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -1,28 +1,37 @@
+/*
+To form all the sequences of balanced bracket subsequences with n pairs. So there are n opening brackets and n closing brackets. 
+So the subsequence will be of length 2*n. There is a simple idea, the i’th character can be ‘{‘ if and only if the count of ‘{‘ till i’th is less than n and i’th character can be ‘}’ if and only if the count of ‘{‘ is greater than the count of ‘}’ till index i. If these two cases are followed then the resulting subsequence will always be balanced. 
+*/
 class Solution {
 public:
     
-    void Solve(int n, string str, int O, int C, vector<string> &ans){
+    void generateParenthesishelper(int n, int OpenCount, int CloseCount, string OutputString, vector<string>& ans){
         
-        if(O == n && C ==n){
-            ans.push_back(str);
+        // BASE CASE
+        if(OutputString.size() == 2*n){
+            ans.push_back(OutputString);
+            return;
         }
         
-        if( O < n){
-            Solve(n, str + '(', O+1, C, ans);
+        
+        // RECURSION CASE
+        
+        if(OpenCount > CloseCount){
+            generateParenthesishelper(n, OpenCount, CloseCount+1, OutputString + ')', ans);
         }
         
-        if( C < O){
-            Solve(n, str + ')', O, C+1, ans);
+        if(OpenCount < n){
+            generateParenthesishelper(n, OpenCount+1, CloseCount, OutputString + '(', ans);
         }
+        
     }
     
-    vector<string> generateParenthesis(int n) {
+        vector<string> generateParenthesis(int n) {
         
         vector<string> ans;
-        string str = "";
+        string OutputString = "";
+        generateParenthesishelper(n,0,0,OutputString,ans);
         
-         Solve(n, str, 0, 0, ans);
-        return ans;
-        
-    }
+            return ans;
+        }
 };
