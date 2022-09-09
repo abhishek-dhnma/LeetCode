aa2edf5cb4a3080a6dd3173ca687d659b0f58a6f
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2022-09-10 02:02:52 +0530

    Time: 9 ms (9.22%), Space: 7.2 MB (99.99%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
@@ -9,78 +9,66 @@
  * };
  */
 class Solution {
-public:
-    
-    ListNode * reverse(ListNode * l, ListNode *  r){
-        
-        r->next = NULL;
-        
-        ListNode * cur = l;
-        ListNode * pre = NULL;
-        while(cur){
-            
-            ListNode* temp = cur->next;
-            cur->next = pre;
-            pre = cur;
-            cur = temp;
+    public:
+
+        ListNode * reverse(ListNode * l, ListNode * r) {
+
+            r -> next = NULL;
+
+            ListNode * cur = l;
+            ListNode * pre = NULL;
+            while (cur) {
+
+                ListNode * temp = cur -> next;
+                cur -> next = pre;
+                pre = cur;
+                cur = temp;
+            }
+
+            return pre;
         }
-        
-        return pre;
-    }
-    
-    
-    
-    ListNode* reverseBetween(ListNode* head, int left, int right) {
-        
-        if(!head || !head->next) return head;
-        //(left == right) return head;
-        
-        ListNode * rhead = NULL;
+
+    ListNode * reverseBetween(ListNode * head, int left, int right) {
+
+        if (!head || !head -> next) return head;
+
         ListNode * tail = head;
         ListNode * l = NULL;
         ListNode * r = NULL;
         ListNode * pleft = NULL;
-        
+
         int i = 1;
-        while(tail ){
-            if(i == left){
+        while (tail) {
+            if (i == left) {
                 l = tail;
             }
-            if(i == right){
+            if (i == right) {
                 r = tail;
             }
-            if(left-1 == i){
+            if (left - 1 == i) {
                 pleft = tail;
             }
             i++;
-            tail = tail->next;
+            tail = tail -> next;
         }
-        
-        ListNode * nright;
-        if(r){
-            nright = r->next;
-        }
-        
-        if(pleft)  pleft->next = NULL;
-        
-        if(l < r){
-        rhead = reverse(l,r);
-        }
-        
-        while(rhead){
-            cout << rhead->val;
-            rhead = rhead->next;
-        }
-        
 
-        
-        
-        l->next = nright;
-        if(pleft) { pleft->next = r;}else{ head = r; }
-        
-       
-        
+        ListNode * nright;
+
+            nright = r -> next;
+
+
+        if (pleft) pleft -> next = NULL;
+
+        reverse(l, r);
+
+        l -> next = nright;
+        if (pleft) {
+            pleft -> next = r;
+        } else {
+            head = r;
+        }
+
         return head;
-        
+
     }
 };
