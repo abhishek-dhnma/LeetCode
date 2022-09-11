808792944cc958abdef7b78eb5636262fa4190ac
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2022-09-12 03:12:57 +0530

    Time: 474 ms (5.01%), Space: 109.7 MB (63.23%) - LeetHub

--- 643-maximum-average-subarray-i/643-maximum-average-subarray-i.cpp
+++ 643-maximum-average-subarray-i/643-maximum-average-subarray-i.cpp
@@ -3,38 +3,29 @@ public:
     double findMaxAverage(vector<int>& nums, int k) {
         
         int n = nums.size();
-        if(k > n){
-            return 0;
-        }
         
         // Sliding Window Technique Question
         
         double ans;
-        
-        
         double sum = 0;
         
         for(int i=0; i<k; i++){
             sum += nums[i];
         }
         
-        double max_avg = sum;
-        max_avg /= k;
-        ans = max_avg;
-        cout << ans << " ";
-        
-        
-        double window_avg = max_avg;
-        for(int j=k; j<n; j++){
-            sum += nums[j] - nums[j-k];
-            window_avg = sum;
-            window_avg /= k;
+        ans = sum;
 
-            ans = max(ans, window_avg);
+        
+        for(int j=k; j<n; j++){
+            ans = max(ans, sum);
+            sum += nums[j] - nums[j-k];
+           
         }
         
+        ans = max(ans, sum);
         
-        return ans;
+        
+        return ans/k;
         
         
     }
