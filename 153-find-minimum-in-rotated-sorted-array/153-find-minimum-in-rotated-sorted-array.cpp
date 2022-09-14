3d65edc55ba3da9ede48534e0a737e34aeb4d03d
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2022-09-15 00:36:52 +0530

    Time: 0 ms (100.00%), Space: 10.3 MB (22.77%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,40 +1,37 @@
 class Solution {
-public:
-    int findMin(vector<int>& arr) {
-        
-        // https://www.callicoder.com/find-minimum-element-in-rotated-sorted-array/
-        int n = arr.size();
-        int s =0;
-    int e = n-1;
+    public:
+        int findMin(vector < int > & arr) {
 
-    if(arr[s] <= arr[e]){
-        return arr[s];
-    }
+            // Check Notes
+            int n = arr.size();
+            int s = 0;
+            int e = n - 1;
 
+            if (arr[s] <= arr[e]) {
+                return arr[s];
+            }
 
-    while(s <= e){
+            while (s <= e) {
 
-        int mid = s + ((e-s)>>1);
+                int mid = s + ((e - s) >> 1);
 
-        if(mid > 0 and arr[mid] < arr[mid-1]){
-                return arr[mid];
+                if (mid > 0 and arr[mid] < arr[mid - 1]) {
+                    return arr[mid];
+
+                }
+                if (mid < n - 1 and arr[mid + 1] < arr[mid]) {
+                    return arr[mid + 1];
+                }
+
+                if (arr[s] <= arr[mid]) {
+                    s = mid + 1;
+                } else {
+                    e = mid - 1;
+                }
+
+            }
+
+            return -1;
 
         }
-         if(mid < n-1 and arr[mid+1] < arr[mid] ){
-             return arr[mid+1];
-        }
-
-
-        if(arr[s] <= arr[mid]){
-            s = mid+1;
-        }else{
-            e = mid-1;
-        }
-
-        
-    }
-
-    return -1;
-        
-    }
 };
