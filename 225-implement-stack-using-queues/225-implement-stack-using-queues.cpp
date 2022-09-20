779db2e14529c9b76660a6ebd6d679c2d5ebeae3
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2022-09-21 02:20:03 +0530

    Time: 7 ms (7.25%), Space: 7 MB (28.07%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
@@ -10,7 +10,7 @@ public:
     
     void push(int x) {
         if(q1.empty() && q2.empty()){
-            q1.push(x);
+            q1.push(x); // or q2.push(x)
         }else if(q1.empty() && !q2.empty()){
             q2.push(x);
         }else{
@@ -21,21 +21,27 @@ public:
     int pop() {
         int temp;
         if(q1.empty() && q2.empty()){
-            // No acton taken
+            // No acton taken 
+            // stack is empty
         }else if(q1.empty() && !q2.empty()){
+            // transfer n-1 elements from q2 to q1
             while(q2.size() > 1){
                 q1.push(q2.front());
                 q2.pop();
             }
+            //Save q2 front in temp According to question we need to return that
             temp = q2.front();
+            // pop stack top i.e. pop the nth element from q2
             q2.pop();
             
         }else{
+            // transfer n-1 elements from q1 to q2
             while(q1.size() > 1){
                 q2.push(q1.front());
                 q1.pop();
             }
             temp = q1.front();
+            // pop stack top i.e. pop the nth element from q1
             q1.pop();
         }
         return temp;
@@ -46,19 +52,22 @@ public:
         if(q1.empty() && q2.empty()){
             
         }else if(q1.empty() && !q2.empty()){
+            // transfer n-1 elements from q2 to q1
             while(q2.size() > 1){
                 q1.push(q2.front());
                 q2.pop();
             }
+            // save & pop the nth element from q2 and transfer it to q1
             temp = q2.front();
             q2.pop();
             q1.push(temp);
         }else{
+            // transfer n-1 elements from q1 to q2
             while(q1.size() > 1){
                 q2.push(q1.front());
                 q1.pop();
             }
-            
+            // save & pop the nth element from q1 and transfer it to q2
             temp = q1.front();
             q1.pop();
             q2.push(temp);
@@ -67,6 +76,7 @@ public:
     }
     
     bool empty() {
+        // If both are empty then Stack is also empty
         return q1.empty() && q2.empty();
     }
 };
