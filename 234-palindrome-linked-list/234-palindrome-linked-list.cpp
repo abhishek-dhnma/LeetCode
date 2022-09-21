1a0c1fa58dac92871c4fa2497c9152c5cd66345a
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2022-09-22 00:36:55 +0530

    Time: 308 ms (71.63%), Space: 116.8 MB (64.91%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -13,9 +13,9 @@ public:
     bool isPalindrome(ListNode* head) {
         
         if(!head || !head->next) return true;
-        
         stack<int> stk;
         
+        // make slow pointer on the mid of the LL
         ListNode* slow = head;
         ListNode* fast = head->next;
         while(fast and fast->next){
@@ -23,14 +23,17 @@ public:
             fast = fast->next->next;
         }
         
+        // Break the links head and head2
         ListNode * head2 = slow->next;
         slow->next = NULL;
         
+        // Push head2 into stack i.e they become reverse LIFO
         while(head2){
             stk.push(head2->val);
             head2 = head2->next;
         }
         
+        // Check stack top is equal to linked list val or not
         while(!stk.empty() and head){
             if(head->val != stk.top()){
                 return false;
