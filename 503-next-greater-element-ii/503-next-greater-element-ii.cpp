e698b3864fe1687271589e91f31bbd1b4db1f7d6
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2022-09-23 02:04:01 +0530

    Time: 24 ms (99.80%), Space: 25.4 MB (11.85%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/503-next-greater-element-ii.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& arr) {
-        int n = arr.size();
+        
+    // CHECK NOTES AND PREVIOUS SOLUTION FOR MORE UNDERSTANDING
+        
+    int n = arr.size();
     vector<int> ans;
     stack<int> s;
-
+    
+    // Already add all the element in the stack in reverse order (n-1 to 0)
     for(int i=n-1; i>=0; i--){
         s.push(arr[i]);
     }
+        
+    // Again Traverse the array and this time we have stack which help in finding the next greater element
+        
+    // TC - O(N) and SC - O(N)
 
     for(int i=n-1; i>=0; i--){
 
