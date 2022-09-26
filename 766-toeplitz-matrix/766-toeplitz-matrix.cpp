5010f292579a4c37a121dbcb71466bb54dc7c77f
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2022-09-27 01:26:14 +0530

    Time: 45 ms (5.47%), Space: 17.5 MB (63.49%) - LeetHub

--- 766-toeplitz-matrix/766-toeplitz-matrix.cpp
+++ 766-toeplitz-matrix/766-toeplitz-matrix.cpp
@@ -2,6 +2,11 @@ class Solution {
 public:
     bool isToeplitzMatrix(vector<vector<int>>& matrix) {
         
+        // CHECK NOTES
+        // https://leetcode.com/problems/toeplitz-matrix/discuss/113422/C%2B%2BJava-Clean-Code (both solution find here)
+    // 1 Way : Skip first row and column. So, we have previous values for checking (Clever Technique)
+    // 2. Way : Check elements diagonally. Upper half and then bottom half
+        
         int n = matrix.size();
         int m = matrix[0].size();
         
