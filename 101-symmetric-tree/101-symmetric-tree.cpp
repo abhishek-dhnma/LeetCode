728bb6d2020f72e937d0eeb605c07b5a72aee11c
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2022-09-29 02:25:29 +0530

    Time: 11 ms (42.55%), Space: 18.9 MB (6.29%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.cpp
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -12,38 +12,40 @@
 class Solution {
 public:
     
-    TreeNode * swapp(TreeNode* root){
-        
-        if(!root) return NULL;
-        
-        TreeNode* newroot = new TreeNode(root->val);
-        newroot->left = swapp(root->left);
-        newroot->right = swapp(root->right);
-        
-        swap(newroot->left, newroot->right);
-        
-        return newroot;
-    }
-    
-    
-    bool check(TreeNode* newroot, TreeNode* root){
-        
-        if(!root and !newroot) return true;
-        if(!root || !newroot) return false;
-        if(root->val != newroot->val) return false;
-        
-        return check(newroot->left,root->left) and check(newroot->right,root->right);
-        
+    // CHECK NOTES FOR SOLUTION
+
+        // Create a new tree which is mirror of given tree
+        TreeNode * swapp(TreeNode * root) {
+
+            if (!root) return NULL;
+
+            TreeNode * newroot = new TreeNode(root -> val);
+            newroot -> left = swapp(root -> left);
+            newroot -> right = swapp(root -> right);
+
+            swap(newroot -> left, newroot -> right);
+
+            return newroot;
+        }
+
+    // LEETCODE 100 (Same Code Used Here) Check Identical and have Same root val
+    bool check(TreeNode * newroot, TreeNode * root) {
+
+        if (!root and!newroot) return true;
+        if (!root || !newroot) return false;
+        if (root -> val != newroot -> val) return false;
+
+        return check(newroot -> left, root -> left) and check(newroot -> right, root -> right);
+
     }
 
-    
-    
-    bool isSymmetric(TreeNode* root) {
-        
-        TreeNode* newroot = swapp(root);
+    // Main Function 
+    bool isSymmetric(TreeNode * root) {
+
+        //Mirrored New Tree
+        TreeNode * newroot = swapp(root);
+        //Check Both are Identical or not
         return check(newroot, root);
-        
-        
 
     }
 };
